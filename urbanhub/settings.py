e38b15274caa19db9d54d97131a752cae54eec06
ansilrahman777 <rahmanansil777@gmail.com>
2025-05-25 21:10:07 +0400

    Prepare for Render deployment

--- urbanhub/settings.py
+++ urbanhub/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -61,6 +63,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'whitenoise.middleware.WhiteNoiseMiddleware',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
@@ -79,15 +82,19 @@ AUTH_USER_MODEL = 'user_side.User'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':config('NAME'),
+#         'USER':config('USER'),
+#         'PASSWORD':config('PASSWORD'),
+#         # 'HOST':'localhost',
+#         # 'PORT':'5432'
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':config('NAME'),
-        'USER':config('USER'),
-        'PASSWORD':config('PASSWORD'),
-        # 'HOST':'localhost',
-        # 'PORT':'5432'
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
@@ -138,13 +145,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =BASE_DIR /'static'
-STATICFILES_DIRS = [
-    'urbanhub/static'
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL='media/'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
