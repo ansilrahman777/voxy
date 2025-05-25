aec1aac7d6fab397bbecfede350be8a9ca47c6c3
ansilrahman777 <rahmanansil777@gmail.com>
2025-05-25 22:11:31 +0400

    static and slash issue

--- urbanhub/settings.py
+++ urbanhub/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=False,cast=bool)
+APPEND_SLASH = True
 
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['voxy.ansilrahman.in', 'voxy-zvj5.onrender.com']
@@ -146,11 +147,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles' 
-
+STATIC_ROOT =BASE_DIR /'static'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    BASE_DIR / 'urbanhub' / 'static',
+    'urbanhub/static'
 ]
 
 
