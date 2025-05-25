0c91688b7544f36a51ae7181324ea3ade32ae2dd
ansilrahman777 <rahmanansil777@gmail.com>
2025-05-25 22:02:05 +0400

    static issue

--- urbanhub/settings.py
+++ urbanhub/settings.py
@@ -146,10 +146,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'urbanhub' / 'static',
+]
 
 
 
